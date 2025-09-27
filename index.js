19aa40d47f7b25cae36b669532263ed48d88c874
MD Emon <96580799+GreatEmon@users.noreply.github.com>
2025-09-27 13:11:17 +0600

    finally all added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ admin.initializeApp({
 
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization
-  const token = authHeader.startsWith('Bearer ') && authHeader.split(' ')[1];
+  const token = authHeader?.startsWith('Bearer ') && authHeader?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'No token provided' });
   const decoded = await admin.auth().verifyIdToken(token);
   req.user = decoded; // contains uid, email, etc.
@@ -40,9 +40,9 @@ const verifyFirebaseToken = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // all route here
@@ -53,10 +53,10 @@ async function run() {
 
     app.get('/', verifyFirebaseToken, async (req, res) => {
 
-      if(req.user.email) {
+      if (req.user.email) {
         const cursor = listcollection.find();
         const data = await cursor.toArray()
-      res.send(data)
+        res.send(data)
       }
 
     })
@@ -71,14 +71,14 @@ async function run() {
       res.send("Server is running")
     })
 
-    app.get('/home', async (req, res) => {
+    app.get('/home', verifyFirebaseToken, async (req, res) => {
       const cursor = listcollection.find().limit(16);
       const data = await cursor.toArray()
       res.send(data)
     })
 
 
-    app.post('/add', async (req, res) => {
+    app.post('/add', verifyFirebaseToken, async (req, res) => {
       const body = req.body;
 
       // force number type
@@ -94,86 +94,111 @@ async function run() {
       res.send(added)
     })
 
-    app.get('/myproducts/:email', async (req, res) => {
-      const email = req.params.email;
-      const query = { userEmail: email }
-      const result = await listcollection.find(query).toArray();
-      res.send(result)
-    })
-
-    app.get('/products/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await listcollection.findOne(query);
-      res.send(result)
-    })
-
-    app.get('/category/:slug', async (req, res) => {
-      const slug1 = req.params.slug;
-      // console.log(slug)
-      const query = { slug: slug1 }
-      const result = await listcollection.find(query).toArray();
-      res.send(result)
-    })
-
-
-    app.patch("/api/products/:id/decrement", async (req, res) => {
-      const { id } = req.params;
-      const { amount } = req.body;
-      const result = await listcollection.updateOne(
-        { _id: new ObjectId(id), stock: { $gt: 0 } }, // prevent negative stock
-        { $inc: { stock: -amount } });
-      // console.log(id, amount)
-      res.send(result)
-    })
-
-    app.patch('/update/:id', async (req, res) => {
-      const id = req.params.id;
-      const body = req.body;
-
-      // force number type
-      const product = {
-        ...body,
-        price: Number(body.price),
-        stock: Number(body.stock),
-        rating: Number(body.rating),
-        minimumOrderQuantity: Number(body.minimumOrderQuantity),
-        discountPercentage: Number(body.discountPercentage)
-      };
-      const query = { _id: new ObjectId(id) }
-      const lookup = await listcollection.findOne(query);
-      const updatedDoc = {
-        $set: product
+    app.get('/myproducts/:email', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email === req.params.email) {
+        const email = req.params.email;
+        const query = { userEmail: email }
+        const result = await listcollection.find(query).toArray();
+        res.send(result)
       }
-      const result = await listcollection.updateOne(lookup, updatedDoc)
-      res.send(result)
+      res.status(401).send({ "messsage": "unauthorized" })
+
+    })
+
+    app.get('/products/:id', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await listcollection.findOne(query);
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
+    })
+
+    app.get('/category/:slug', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const slug1 = req.params.slug;
+        // console.log(slug)
+        const query = { slug: slug1 }
+        const result = await listcollection.find(query).toArray();
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
     })
 
 
-    app.post('/cart/add', async (req, res) => {
-      const added = await cart.insertOne(req.body)
-      res.send(added)
+    app.patch("/api/products/:id/decrement", verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const { id } = req.params;
+        const { amount } = req.body;
+        const result = await listcollection.updateOne(
+          { _id: new ObjectId(id), stock: { $gt: 0 } }, // prevent negative stock
+          { $inc: { stock: -amount } });
+        // console.log(id, amount)
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
     })
 
-    app.get('/cart/:email', async (req, res) => {
-      const email = req.params.email;
-      const query = { userEmail: email }
-      const result = await cart.find(query).toArray();
-      res.send(result)
+    app.patch('/update/:id', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const id = req.params.id;
+        const body = req.body;
+
+        // force number type
+        const product = {
+          ...body,
+          price: Number(body.price),
+          stock: Number(body.stock),
+          rating: Number(body.rating),
+          minimumOrderQuantity: Number(body.minimumOrderQuantity),
+          discountPercentage: Number(body.discountPercentage)
+        };
+        const query = { _id: new ObjectId(id) }
+        const lookup = await listcollection.findOne(query);
+        const updatedDoc = {
+          $set: product
+        }
+        const result = await listcollection.updateOne(lookup, updatedDoc)
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
+    })
+
+
+    app.post('/cart/add', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const added = await cart.insertOne(req.body)
+        res.send(added)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
+    })
+
+    app.get('/cart/:email', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email === req.params.email) {
+        const email = req.params.email;
+        const query = { userEmail: email }
+        const result = await cart.find(query).toArray();
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
     })
 
 
 
-    app.delete('/delete/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result1 = await cart.find(query).toArray();
-      const amount = Number(result1[0].orderQuantity);
-      const result2 = await listcollection.updateOne(
-        { _id: new ObjectId(result1[0].id) },
-        { $inc: { stock: amount } });
-      const result = await cart.deleteOne(query);
-      res.send(result)
+    app.delete('/delete/:id', verifyFirebaseToken, async (req, res) => {
+      if (req.user.email) {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result1 = await cart.find(query).toArray();
+        const amount = Number(result1[0].orderQuantity);
+        const result2 = await listcollection.updateOne(
+          { _id: new ObjectId(result1[0].id) },
+          { $inc: { stock: amount } });
+        const result = await cart.deleteOne(query);
+        res.send(result)
+      }
+      res.status(401).send({ "messsage": "unauthorized" })
     })
 
 
